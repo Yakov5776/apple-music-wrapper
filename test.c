7bf01f18a7b46bfa187c5bb43b6738db78339579
itouakirai <85016486+itouakirai@users.noreply.github.com>
2024-09-06 20:00:17 +0800

    feat: Disable getting m3u8 feature defaultly

--- test.c
+++ test.c
@@ -122,7 +122,7 @@ static inline void init() {
         setenv("https_proxy", args_info.proxy_arg, 1);
     }
 
-    static const char *resolvers[2] = {"1.1.1.1", "119.29.29.29"};
+    static const char *resolvers[2] = {"1.1.1.1", "1.0.0.1"};
     _resolv_set_nameservers_for_net(0, resolvers, 2, ".");
 #ifndef MyRelease
     subhook_install(subhook_new(
@@ -544,7 +544,11 @@ int main(int argc, char *argv[]) {
     _ZN22SVPlaybackLeaseManager12requestLeaseERKb(leaseMgr, &autom);
     FHinstance = _ZN21SVFootHillSessionCtrl8instanceEv();
 
-    pthread_t m3u8_thread;
-    pthread_create(&m3u8_thread, NULL, &new_socket_m3u8, NULL);
+    if (args_info.m3u8_port_given) {
+        pthread_t m3u8_thread;
+        pthread_create(&m3u8_thread, NULL, &new_socket_m3u8, NULL);
+    } else {
+        fprintf(stderr, "[!] The feature of getting m3u8 is defaultly disabled because it's unstable now. To enable it, please manually specify m3u8-port param.\n");
+    }
     return new_socket();
 }
